865d0f379c4d8116be8de0d68635be1860ad1d41
Linar <linarka94@gmail.com>
2020-09-03 12:57:02 +0300

    check

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-const burgerMenu = document.getElementById('burgerMenu');
-let body = document.querySelector('body');
-
-burgerMenu.onclick = function() {
-  burgerMenu.classList.toggle('burger__label--show');
-  body.classList.toggle('body--overflow-hidden')
-}
